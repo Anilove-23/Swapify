f99e067a8eafdd7215e2160216dcec0fe46f17f6
Anilove <anilovejee@gmail.com>
2025-07-14 17:05:25 +0530

    did some work of dashboard upar upar se

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const data = localStorage.getItem("userProfile");
   if (data) {
     const { name, bio, teach, learn } = JSON.parse(data);
+    document.getElementById("welcome").textContent = "Welcome " + name;
+
     document.getElementById("dash-name").textContent = name;
     document.getElementById("dash-bio").textContent = bio;
     document.getElementById("dash-teach").textContent = teach.join(", ");
